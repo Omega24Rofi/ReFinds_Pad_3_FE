4c14e7ab4fb1327269a21300c795bdde84aaf7f6
WahyuAgg <wahyuagungbarokah@mail.ugm.ac.id>
2024-10-04 10:20:18 +0700

    login_register

--- src/app/pages/auth/login/page.jsx
+++ src/app/pages/auth/login/page.jsx
@@ -1,30 +1,91 @@
-import React from 'react';
+"use client";
+import React from "react";
 import "../../../globals.css";
-import Image from 'next/image';
+import Image from "next/image";
+
+// be
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:8000/api/login", {
+        email,
+        password: password,
+      });
+
+      // Save token in localStorage
+      localStorage.setItem("token", response.data.token);
+
+      setMessage("Login successful");
+      setError("");
+    } catch (err) {
+      setError("Login failed");
+      setMessage("");
+    }
+  };
+
   return (
-    <div className='bg-lightbg min-h-screen flex items-center justify-center'>
-        <div className='container w-2/3 h-96 bg-white flex items-start '>
-            <div className="logside w-2/3 p-8">
-                <h1 className='font-bold text-2xl mb-4'>Login</h1>
-                <form action="" method="post">
-                  <label htmlFor="email" className='block mb-4'>
-                    Email
-                    <input type="email" name="email" id="email" placeholder='email' className='border p-2 w-full mt-2'/>
-                  </label>
-                  <label htmlFor="password" className='block mb-4'>
-                    Password
-                    <input type="password" name="password" id="password" placeholder='password' className='border p-2 w-full mt-2'/>
-                  </label>
-                </form>
-            </div>
-            <div className="colorcard w-1/3 bg-login-gradient h-full flex items-center justify-center">
-                <img src="/images/Logo-White.svg" alt="Refinds Logo" className='w-2/5'/>
-            </div>
+    <div className="bg-lightbg min-h-screen flex items-center justify-center">
+      <div className="container w-2/3 h-96 bg-white flex items-start ">
+        <div className="logside w-2/3 p-8">
+          <h1 className="font-bold text-2xl mb-4">Login</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email" className="block mb-4">
+              Email
+              <input
+                type="email"
+                id="email"
+                placeholder="email"
+                className="border p-2 w-full mt-2"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <label htmlFor="password" className="block mb-4">
+              Password
+              <input
+                type="password"
+                id="password"
+                placeholder="password"
+                className="border p-2 w-full mt-2"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+
+            {/* tombol login */}
+            <button
+              type="submit"
+              className="mt-4 bg-blue-500 text-white py-2 px-4 rounded"
+            >
+              Login
+            </button>
+            <br />
+            {message && <p>{message}</p>}
+            {error && <p>{error}</p>}
+          </form>
         </div>
+        <div className="colorcard w-1/3 bg-login-gradient h-full flex items-center justify-center">
+          <img
+            src="/images/Logo-White.svg"
+            alt="Refinds Logo"
+            className="w-2/5"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
--- src/app/pages/auth/register/page.jsx
+++ src/app/pages/auth/register/page.jsx
@@ -1,38 +1,147 @@
-import React from 'react';
+"use client";
+
+import React from "react";
 import "../../../globals.css";
-import Image from 'next/image';
+import Image from "next/image";
+
+//
+import { useState } from "react";
+import axios from "axios";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 const Register = () => {
+  const [namaAkun, setNamaAkun] = useState("");
+  const [namaAsliUser, setNamaAsliUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  // fungsi untuk handle submission
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent form from default refresh
+
+    try {
+      // mengirim data menggunakan axios ke API
+      // menunggu response dari exios
+      const response = await axios.post("http://localhost:8000/api/register", {
+        nama_akun: namaAkun,
+        nama_asli_user: namaAsliUser,
+        email, // short syntaxt bc nama var sama
+        password,
+        password_confirmation: passwordConfirmation,
+      });
+
+      // mengambil respons pesan dari BE
+      setMessage(response.data.message); // Set success message
+      setError(""); // Reset error
+    } catch (err) {
+      if (err.response && err.response.data) {
+        const errorMessage =
+          err.response.data.message || "Terjadi kesalahan, silakan coba lagi.";
+        setError(errorMessage);
+      } else {
+        setError("Terjadi kesalahan, silakan coba lagi.");
+      }
+      setMessage(""); // Reset success message
+    }
+  };
+
   return (
-    <div className='bg-lightbg min-h-screen flex items-center justify-center'>
-        <div className='container w-2/3 h-96 bg-white flex items-start '>
-            <div className="logside w-2/3 p-8">
-                <h1 className='font-bold text-2xl mb-4'>Register</h1>
-                <form action="" method="post">
-                  <label htmlFor="email" className='block mb-4'>
-                    Email
-                    <input type="email" name="email" id="email" placeholder='email' className='border p-2 w-full mt-2'/>
-                  </label>
-                  <label htmlFor="password" className='block mb-4'>
-                    Password
-                    <input type="password" name="password" id="password" placeholder='password' className='border p-2 w-full mt-2'/>
-                  </label>
-                  <label htmlFor="Username" className='block mb-4'>
-                    Password
-                    <input type="text" name="password" id="password" placeholder='password' className='border p-2 w-full mt-2'/>
-                  </label>
-                  <label htmlFor="nama" className='block mb-4'>
-                    Password
-                    <input type="text" name="password" id="password" placeholder='password' className='border p-2 w-full mt-2'/>
-                  </label>
-                </form>
-            </div>
-            <div className="colorcard w-1/3 bg-login-gradient h-full flex items-center justify-center">
-                <img src="/images/Logo-White.svg" alt="Refinds Logo" className='w-2/5'/>
-            </div>
+    <div className="bg-lightbg min-h-screen flex items-center justify-center">
+      <div className="container w-2/3 h-auto bg-white flex items-start">
+        <div className="logside w-2/3 p-8">
+          <h1 className="font-bold text-2xl mb-4">Register</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email" className="block mb-4">
+              Email
+              <input
+                type="email"
+                placeholder="email"
+                className="border p-2 w-full mt-2"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+
+            <label htmlFor="password" className="block mb-4">
+              Password
+              <input
+                type="password"
+                placeholder="password"
+                className="border p-2 w-full mt-2"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+
+            <label htmlFor="passwordConfirmation" className="block mb-4">
+              Password
+              <input
+                type="password"
+                placeholder="setPasswordConfirmation"
+                className="border p-2 w-full mt-2"
+                id="passwordConfirmation"
+                value={passwordConfirmation}
+                onChange={(e) => setPasswordConfirmation(e.target.value)}
+                required
+              />
+            </label>
+
+            <label htmlFor="username" className="block mb-4">
+              Username
+              <input
+                type="text"
+                placeholder="username"
+                className="border p-2 w-full mt-2"
+                id="namaAkun"
+                value={namaAkun}
+                onChange={(e) => setNamaAkun(e.target.value)}
+                required
+              />
+            </label>
+            <label htmlFor="name" className="block mb-4">
+              Name
+              <input
+                type="text"
+                placeholder="name"
+                className="border p-2 w-full mt-2"
+                id="namaAsliUser"
+                value={namaAsliUser}
+                onChange={(e) => setNamaAsliUser(e.target.value)}
+                required
+              />
+            </label>
+
+            {/* tombol register */}
+            <button
+              type="submit"
+              className="mt-4 bg-blue-500 text-white py-2 px-4 rounded"
+            >
+              Register
+            </button>
+            <br />
+            {message && <p>{message}</p>}
+            {error && <p>{error}</p>}
+          </form>
         </div>
+        <div className="colorcard w-1/3 bg-login-gradient h-full flex items-center justify-center">
+          <img
+            src="/images/Logo-White.svg"
+            alt="Refinds Logo"
+            className="w-2/5"
+          />
+        </div>
+        
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
 export default Register;
