4696c9e9a6f9a5d618a9cb5a2dd6477cc1e5fa1c
Omega24Rofi <iamrofif@gmail.com>
2024-10-18 14:16:06 +0700

    add minor code

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,7 +5,7 @@ const header = () => {
   return (
     <header className='w-full h-min  bg-lightbluemain py-2 px-20'>
       <div className="container flex flex-row align-middle justify-evenly">
-        <div className='flex flex-row justify-evenly align-middle w-1/4 h-full'>
+        <div className='flex justify-evenly align-bottom w-1/4 h-full bg-[red]'>
             <img src="/images/Logo-blue.png" alt="logo-refinds-blue" className='h-12 w-auto'/>
             <img src="/images/text-logosvg.svg" alt="logo-refinds-type" className='w-auto h-6' />
         </div>
