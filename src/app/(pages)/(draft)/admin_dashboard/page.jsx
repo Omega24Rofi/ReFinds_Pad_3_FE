a4d9f1a43120374d70803bb3cd1b397db696e53a
Omega24Rofi <iamrofif@gmail.com>
2024-12-06 11:45:37 +0700

    add popup to admin

--- src/app/(pages)/(draft)/admin_dashboard/page.jsx
+++ src/app/(pages)/(draft)/admin_dashboard/page.jsx
@@ -3,11 +3,41 @@
 import { useEffect, useState } from "react";
 import api from "@/utils/axios";
 import useKategori from "@/hooks/useKategori";
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import LazyLoad from "react-lazyload";
+
 
 const ProdukList = () => {
   const { kategoriData } = useKategori();
-  console.log("KATEGORI_DATA:", kategoriData);
+  const [isDetailOpen, setIsDetailOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
+
+  const openDetail = (produk) => {
+    setSelectedProduct(produk); // Set the selected product
+    setIsDetailOpen(true); // Open the modal
+  };
+
+  const closeDetail = () => {
+    setSelectedProduct(null); // Clear the selected product
+    setIsDetailOpen(false); // Close the modal
+  };
+  
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyLoad: 'ondemand', 
+    adaptiveHeight: true, 
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true, 
+  };
 
 
   const handleAccProduk = async (id_produk, isAccepted) => {
@@ -140,13 +170,45 @@ const ProdukList = () => {
                   <p>Nama Produk: {produk.nama_produk}</p>
                   <p>Harga: Rp {produk.harga}</p>
                   <p>Deskripsi produk: {produk.deskripsi}</p>
+                  {isDetailOpen && selectedProduct && (
+  <div className="absolute min-h-screen  inset-0 bg-black bg-opacity-15 z-[999]">
+    <div className="w-1/3 mt-24 mx-auto bg-white rounded-lg shadow-lg p-6 flex flex-col items-center">
+      <h2 className="text-xl font-semibold mb-4">Detail Produk</h2>
+      <div className="product-carousel mb-4 flex flex-row overflow-scroll gap-2">
+        {selectedProduct.list_url_gambar.map((img, index) => (
+          <div key={index} >
+            <LazyLoad width={400}  offset={100}>
+              <img src={img} alt={`Product ${index + 1}`} className="product-image " />
+            </LazyLoad>
+          </div>
+        ))}
+      </div>
+      <div className="product-details text-left w-full">
+        <h3 className="font-bold">Nama Produk:</h3>
+        <p>{selectedProduct.nama_produk}</p>
+
+        <h3 className="font-bold mt-4">Harga Produk:</h3>
+        <p>{selectedProduct.harga}</p>
+
+        <h3 className="font-bold mt-4">Deskripsi Produk:</h3>
+        <p>{selectedProduct.deskripsi}</p>
+
+        <h3 className="font-bold mt-4">Detail Penjual:</h3>
+        <p><strong>Nama Penjual:</strong> {selectedProduct.user.nama_akun}</p>
+      </div>
+      <button className="mx-auto px-4 py-2 text-white rounded-lg text-2xl bg-blue_btn hover:bg-blue_btn_hover" onClick={closeDetail}>
+        Kembali
+      </button>
+    </div>
+  </div>
+)}
                 </div>
 
                 {/* Tombol untuk menerima atau menolak produk */}
                 <div className="flex mr-2 items-center justify-center gap-2 my-auto">
-                  <button>
-                    <img src="/icons/info.svg" alt="info" />
-                  </button>
+                <button onClick={() => openDetail(produk)}>
+                <img src="/icons/info.svg" alt="info" />
+                </button>
                   <button
                     className="button bg-blue_btn text-white text-lg font-bold w-28 py-2 rounded-xl"
                     onClick={() => handleAccProduk(produk.id_produk, true)}
@@ -159,6 +221,7 @@ const ProdukList = () => {
                   >
                     Tolak
                   </button>
+
                 </div>
               </div>
             </li>
